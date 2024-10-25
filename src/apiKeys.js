5843fda8a7388245e30c9e684f5828e8f50e62d5
H4rshcodes <jstharsh17@gmail.com>
2024-10-25 20:18:16 +0530

    first changes

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "0e6fae478a0a5a3b7bda1fba306d6ad8",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
